bpastene/python-adb

commit b17110a0c90c8e2defa4d0fd414e9ef780231c28

Migrate high.py and adb_commands_safe.py to contrib.
This is in preparation for upstreaming, as these are not yet ready for broader
consumption.

adb/contrib/high.py
@@ -31,10 +31,10 @@ import threading
 import time
 
 
-from adb import adb_commands_safe
 from adb import common
 from adb import parallel
 from adb import sign_pythonrsa
+from adb.contrib import adb_commands_safe
 
 
 ### Private stuff.

high_test.py
@@ -18,7 +18,7 @@ import sys
 import unittest
 
 
-from adb import high
+from adb.contrib import high
 
 
 class MockDevice(object):

parallel_test.py
@@ -19,9 +19,7 @@ import sys
 import unittest
 
 
-THIS_DIR = os.path.dirname(os.path.abspath(__file__))
-sys.path.insert(0, os.path.dirname(THIS_DIR))
-from adb import parallel
+from adb.contrib import parallel
 
 
 class Foo(Exception):

smoke_test.py
@@ -34,11 +34,11 @@ import unittest
 import usb1
 
 
-from adb import adb_commands_safe
 from adb import adb_protocol
 from adb import common
-from adb import high
 from adb import usb_exceptions
+from adb.contrib import adb_commands_safe
+from adb.contrib import high
 
 
 class Filter(object):

commit 346e6b33332ab9666196572a63126e82b1de048b

Fix regression introduced in b17110a0c90c8e2defa4d0fd414e9ef780231c28.
Forgot to update one import.

adb/contrib/high.py
@@ -32,9 +32,9 @@ import time
 
 
 from adb import common
-from adb import parallel
 from adb import sign_pythonrsa
 from adb.contrib import adb_commands_safe
+from adb.contrib import parallel
 
 
 ### Private stuff.